8b0675641c3415d7411288a69ba7a13fb21cf694
Chilung Wang <chilung.cs06g@nctu.edu.tw>
2020-12-12 14:32:46 +0800

    Update super_resolve.py

--- super_resolve.py
+++ super_resolve.py
@@ -5,7 +5,8 @@ from PIL import Image, ImageDraw, ImageFont
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Model checkpoints
-srgan_checkpoint = "./checkpoint_srgan.pth.tar"
+srgan_checkpoint = 'checkpoint_5400_srgan.pth.tar'
+# srgan_checkpoint = "./checkpoint_srgan.pth.tar"
 srresnet_checkpoint = "./checkpoint_srresnet.pth.tar"
 
 print('cuda: {}, srgan: {} srresnet: {}'.format(device, srgan_checkpoint, srresnet_checkpoint))
