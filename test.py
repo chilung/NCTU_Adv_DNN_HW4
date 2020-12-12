0d1823b845da52bda95774e9baddab68170f6481
Chilung Wang <chilung.cs06g@nctu.edu.tw>
2020-12-12 13:34:39 +0800

    Update test.py

--- test.py
+++ test.py
@@ -9,3 +9,9 @@ srgan_checkpoint = "./checkpoint_srgan.pth.tar"
 srresnet_checkpoint = "./checkpoint_srresnet.pth.tar"
 
 print('cuda: {}, srgan: {} srresnet: {}'.format(device, srgan_checkpoint, srresnet_checkpoint))
+
+# Load models
+srresnet = torch.load(srresnet_checkpoint)['model'].to(device)
+srresnet.eval()
+srgan_generator = torch.load(srgan_checkpoint)['generator'].to(device)
+srgan_generator.eval()
