1401621f5ab44f2c7a74616e80492864bfe47c36
Chilung Wang <chilung.cs06g@nctu.edu.tw>
2020-12-12 13:27:48 +0800

    Update super_resolve.py

--- super_resolve.py
+++ super_resolve.py
@@ -8,6 +8,8 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 srgan_checkpoint = "./checkpoint_srgan.pth.tar"
 srresnet_checkpoint = "./checkpoint_srresnet.pth.tar"
 
+print('cuda: {}, srgan: {} srresnet: {}'.format(device, srgan_checkpoint, srresnet_checkpoint))
+
 # Load models
 srresnet = torch.load(srresnet_checkpoint)['model'].to(device)
 srresnet.eval()
