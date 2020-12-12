1fbbe7aa3ca156e1454e44ac0a87c7233285ce6e
Chilung Wang <chilung.cs06g@nctu.edu.tw>
2020-12-12 13:33:36 +0800

    Update test.py

--- test.py
+++ test.py
@@ -3,3 +3,9 @@ from utils import *
 from PIL import Image, ImageDraw, ImageFont
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# Model checkpoints
+srgan_checkpoint = "./checkpoint_srgan.pth.tar"
+srresnet_checkpoint = "./checkpoint_srresnet.pth.tar"
+
+print('cuda: {}, srgan: {} srresnet: {}'.format(device, srgan_checkpoint, srresnet_checkpoint))
