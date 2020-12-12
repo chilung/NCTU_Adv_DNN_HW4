743613507a0d209216c4d263674e868b2f8fabb3
Chilung Wang <chilung.cs06g@nctu.edu.tw>
2020-12-12 13:30:52 +0800

    Update super_resolve.py

--- super_resolve.py
+++ super_resolve.py
@@ -1,6 +1,6 @@
 import torch
 from utils import *
-# from PIL import Image, ImageDraw, ImageFont
+from PIL import Image, ImageDraw, ImageFont
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
