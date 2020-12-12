1366e9a258736de5a59e970315bf03739db7022b
Chilung Wang <chilung.cs06g@nctu.edu.tw>
2020-12-12 14:37:35 +0800

    Update super_resolve.py

--- super_resolve.py
+++ super_resolve.py
@@ -109,7 +109,7 @@ if __name__ == '__main__':
     # grid_img = visualize_sr('./training_hr_images/8049.png')
     # grid_img.save('./output.png')
     
-    lr_img = Image.open('./testing_hr_images/00.png', mode="r")
+    lr_img = Image.open('./testing_lr_images/00.png', mode="r")
     lr_img = lr_img.convert('RGB')
     
     sr_img_srgan = srgan_generator(convert_image(lr_img, source='pil', target='imagenet-norm').unsqueeze(0).to(device))
