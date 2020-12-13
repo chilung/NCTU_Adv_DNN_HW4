001496b2eb264301b51e0b7f8d95778430feff27
Chilung Wang <chilung.cs06g@nctu.edu.tw>
2020-12-13 22:13:50 +0800

    Update train_srgan.py

--- train_srgan.py
+++ train_srgan.py
@@ -25,7 +25,7 @@ n_blocks_d = 8  # number of convolutional blocks
 fc_size_d = 1024  # size of the first fully connected layer
 
 # Learning parameters
-checkpoint = None  # path to model (SRGAN) checkpoint, None if none
+checkpoint = 'checkpoint_10500_srgan.pth.tar'  # path to model (SRGAN) checkpoint, None if none
 batch_size = 16  # batch size
 start_epoch = 0  # start at this epoch
 iterations = 2e5  # number of training iterations
