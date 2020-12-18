a111a6c67765d7074bf6984f5c4c1d9d541db903
Chilung Wang <chilung.cs06g@nctu.edu.tw>
2020-12-18 20:02:19 +0800

    Update output_sr.py

--- output_sr.py
+++ output_sr.py
@@ -12,7 +12,8 @@ output_path = '/content/drive/MyDrive/NCTU/基於深度學習之視覺辨識專�
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Model checkpoints
-srgan_checkpoint = '/content/drive/MyDrive/NCTU/基於深度學習之視覺辨識專論/HW/HW4/checkpoint_3x_2/checkpoint_37000_srgan.pth.tar'
+# srgan_checkpoint = '/content/drive/MyDrive/NCTU/基於深度學習之視覺辨識專論/HW/HW4/checkpoint_3x_2/checkpoint_37000_srgan.pth.tar'
+srgan_checkpoint = './checkpoint_3x_2/checkpoint_37000_srgan.pth.tar'
 print('device: {}, srgan: {} '.format(device, srgan_checkpoint))
 
 # Load models
