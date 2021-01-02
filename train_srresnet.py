dfa51782d796cff074cd63ee1718f1a21e001df3
Chilung Wang <chilung.cs06g@nctu.edu.tw>
2021-01-02 22:52:32 +0800

    Update train_srresnet.py

--- train_srresnet.py
+++ train_srresnet.py
@@ -98,7 +98,7 @@ def main():
               epoch=epoch)
 
         # Save checkpoint
-        if epoch % 100 == 0:
+        if epoch % 10 == 0:
             torch.save({'epoch': epoch,
                     'model': model,
                     'optimizer': optimizer},
