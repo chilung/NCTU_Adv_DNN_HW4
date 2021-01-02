257cc255b97e396e98b40959c07aedb54cf4b7f5
Chilung Wang <chilung.cs06g@nctu.edu.tw>
2021-01-02 23:02:57 +0800

    Update train_srgan.py

--- train_srgan.py
+++ train_srgan.py
@@ -86,7 +86,7 @@ def main():
                               scaling_factor=scaling_factor)
 
         # Initialize generator network with pretrained SRResNet
-        generator.initialize_with_srresnet(srresnet_checkpoint=os.path.join(args.root, srresnet_checkpoint)l
+        generator.initialize_with_srresnet(srresnet_checkpoint=os.path.join(args.root, srresnet_checkpoint))
 
         # Initialize generator's optimizer
         optimizer_g = torch.optim.Adam(params=filter(lambda p: p.requires_grad, generator.parameters()),
