2e04c9939b21f9d207729f54c34dd35f072c8d4f
Chilung Wang <chilung.cs06g@nctu.edu.tw>
2021-01-04 16:27:03 +0800

    Update output_sr.py

--- output_sr.py
+++ output_sr.py
@@ -9,11 +9,13 @@ import datetime
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('-o', '--output', help='the path to the output directory of super resolution images.')
-parser.add_argument('-g', '--gan', help='the full file path to the super resolution model checkpoint, such as checkpoint_srgan.pth.tar')
+parser.add_argument('-g', '--gan', help='the full file path to the SRGAN super resolution model checkpoint, such as checkpoint_srgan.pth.tar')
+parser.add_argument('-r', '--resnet', help='the full file path to the SRResNetsuper resolution model checkpoint, such as checkpoint_srresnet.pth.tar')
 
 args = parser.parse_args()
 print('output: {}'.format(args.output))
-print('gan model: {}'.format(args.gan))
+print('srgan model: {}'.format(args.gan))
+print('srresnet model: {}'.format(args.resnet))
 
 testing_path = './testing_lr_images'
 output_path = args.output
@@ -24,7 +26,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 srgan_checkpoint = args.gan
 print('device: {}, srgan: {} '.format(device, srgan_checkpoint))
 
-# Load models
+# Load Gan models
 srgan_generator = torch.load(srgan_checkpoint)['generator'].to(device)
 srgan_generator.eval()
 
@@ -33,7 +35,30 @@ testing_files.sort()
 print('number of testing samples: {}'.format(len(testing_files)))
 print('list of testing samples: {}'.format(testing_files))
 
-os.makedirs(output_path, exist_ok=True)
+os.makedirs(output_path+'/gan', exist_ok=True)
+
+for testing_file in testing_files:
+    lr_img = Image.open(join(testing_path, testing_file), mode="r")
+    lr_img = lr_img.convert('RGB')
+    print('processing: {}'.format(testing_file), end = '')
+    
+    sr_img_srgan = srgan_generator(convert_image(lr_img, source='pil', target='imagenet-norm').unsqueeze(0).to(device))
+    sr_img_srgan = sr_img_srgan.squeeze(0).cpu().detach()
+    sr_img_srgan = convert_image(sr_img_srgan, source='[-1, 1]', target='pil')
+    
+    sr_img_srgan.save(join(output_path, testing_file))
+    print(' Done')
+    
+# Load srresnet models
+srgan_generator = torch.load(srgan_checkpoint)['generator'].to(device)
+srgan_generator.eval()
+
+testing_files = [f for f in listdir(testing_path) if isfile(join(testing_path, f))]
+testing_files.sort()
+print('number of testing samples: {}'.format(len(testing_files)))
+print('list of testing samples: {}'.format(testing_files))
+
+os.makedirs(output_path+'/gan', exist_ok=True)
 
 for testing_file in testing_files:
     lr_img = Image.open(join(testing_path, testing_file), mode="r")
