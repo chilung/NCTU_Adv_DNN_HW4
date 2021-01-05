71a3062ae17a25a2bb0839c01d7460efefef6953
Chilung Wang <chilung.cs06g@nctu.edu.tw>
2021-01-05 09:11:47 +0800

    Update train_srgan.py

--- train_srgan.py
+++ train_srgan.py
@@ -164,7 +164,7 @@ def main():
               epoch=epoch)
 
         # Save checkpoint
-        if epoch % 100 == 0:
+        if epoch % 10 == 0:
             torch.save({'epoch': epoch,
                     'generator': generator,
                     'discriminator': discriminator,
